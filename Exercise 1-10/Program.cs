5832d805024ed688b0bb474a2d4bfef54c6746f1
jj-1996 <39858525+jj-1996@users.noreply.github.com>
2018-06-02 16:35:59 +0100

    Solution commit

---
+++ Exercise 1-10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise_1_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = 2;
+            int numMembers = 100;
+
+            for(int i = 0; i < numMembers; i++)
+            {
+                int num = startingNumber + i;
+
+                if((i % 2) != 0)
+                {
+                    num = 0 - num;
+                }
+
+                Console.Write(num.ToString());
+
+                if (i < (numMembers - 1)) Console.Write(", ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
